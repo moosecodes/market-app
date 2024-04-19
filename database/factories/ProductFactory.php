55a92df1eaa2cc2399383c3e4cf7c0386d8b583b
Moose <moosecodes@gmail.com>
2024-04-19 09:50:32 -0700

    Create products table, seeder, factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'sku' => $this->faker->unique()->uuid,
+            'name' => $this->faker->firstName,
+            'description' => $this->faker->realText,
+            'category' => $this->faker->word,
+            'price' => $this->faker->randomFloat(2, 1, 100),
+            'quantity' => $this->faker->numberBetween(1, 100),
+        ];
+    }
+}
