3b93c9ac596f9b554859cf75c21e36812621b27f
Moose <moosecodes@gmail.com>
2024-04-23 00:19:22 -0700

    make name field unique in products table

--- database/migrations/2024_04_19_150153_create_products_table.php
+++ database/migrations/2024_04_19_150153_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('sku')->unique();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->text('description')->nullable();
             $table->string('category');
             $table->decimal('price', 8, 2);
